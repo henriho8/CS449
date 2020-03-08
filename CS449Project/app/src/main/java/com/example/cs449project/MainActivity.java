4039b725accbb4bef939184052e46dde5fa06856
henriho8 <henrivho@gmail.com>
2020-03-08 00:47:41 -0600

    iteration2
    iteration2

--- CS449Project/app/src/main/java/com/example/cs449project/MainActivity.java
+++ CS449Project/app/src/main/java/com/example/cs449project/MainActivity.java
@@ -1,5 +1,28 @@
 package com.example.cs449project;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+// import com.example.lab2_mobiledevelopment.Fragment.UserProfileFragment;
+import com.google.android.gms.common.Feature;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +33,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater acc_inflater = getMenuInflater();
+        acc_inflater.inflate(R.menu.signout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        FirebaseAuth acc_a = FirebaseAuth.getInstance();
+        switch(item.getItemId()){
+            case R.id.Sign_out:
+                acc_a.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
